4c67a5e35311a7d304bb7cf37d31aa8824de5595
sunqiyang <sqy56vue@163.com>
2021-08-17 16:04:12 +0800

    数据修改

--- src/layout/components/Settings/index.vue
+++ src/layout/components/Settings/index.vue
@@ -26,22 +26,5 @@ export default {
   font-size: 14px;
   line-height: 1.5;
   word-wrap: break-word;
-
-  .drawer-title {
-    margin-bottom: 12px;
-    color: rgba(0, 0, 0, .85);
-    font-size: 14px;
-    line-height: 22px;
-  }
-
-  .drawer-item {
-    color: rgba(0, 0, 0, .65);
-    font-size: 14px;
-    padding: 12px 0;
-  }
-
-  .drawer-switch {
-    float: right
-  }
 }
 </style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,3 +1,19 @@
 <template>
     <div>77777777777777777777</div>
 </template>
+<script>
+export default {
+    name:'dashboard',
+    data(){
+        return {
+
+        }
+    },
+    created(){
+        console.log(process.env)
+    },
+    mounted(){
+        console.log(process.env.VUE_APP_BASE_API)
+    }
+}
+</script>
